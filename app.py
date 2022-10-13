52107d543a6dd58e2e1ebd100a82671ab302dad0
Vraj <vraj04.patel@gmail.com>
2022-10-13 22:55:24 +0530

    Final Commmit

---
+++ app.py
@@ -0,0 +1,72 @@
+from distutils.file_util import move_file
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)),
+                        reverse=True, key=lambda x: x[1])[1:6]
+
+    recommend_movies = []
+    recommend_movies_poster = []
+
+    for i in movie_list:
+        #         print(i[0])
+        # print(movies.iloc[i[0]].title)
+        movie_id = movies.iloc[i[0]].movie_id
+        recommend_movies.append(movies.iloc[i[0]].title)
+        # fetch poster from API
+        recommend_movies_poster.append(fetch_poster(movie_id))
+
+    return recommend_movies, recommend_movies_poster
+
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(
+        movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+
+st.title('Movie Recommender System')
+
+selected_movie_name = st.selectbox(
+    'How would you like to be contacted?',
+    movies['title'].values)
+
+if st.button("Recommend"):
+    name, poster = recommend(selected_movie_name)
+    # for i in recommendation:
+    #     st.write(i)
+    # st.write(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(name[0])
+        st.image(poster[0])
+    with col2:
+        st.text(name[1])
+        st.image(poster[1])
+
+    with col3:
+        st.text(name[2])
+        st.image(poster[2])
+    with col4:
+        st.text(name[3])
+        st.image(poster[3])
+    with col5:
+        st.text(name[4])
+        st.image(poster[4])
